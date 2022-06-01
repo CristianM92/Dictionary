8bff00df38a7b9b8c55cc5608cc3770a3220727f
CristianM92 <97915515+CristianM92@users.noreply.github.com>
2022-06-01 20:31:37 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,22 @@
+let added_Words = [];
+function checkWords() {
+    let word = document.getElementById("checkWord").value;
+    if (added_Words.includes(word)) {
+        message.innerHTML = "The word was already added. Please enter another word!";
+        document.getElementById("checkWord").value = "";
+    } else {
+        message.innerHTML = "Word can be added in the dictionary!";
+        document.getElementById("checkWord").value = "";
+    }
+}
+function addWords() {
+    let word = document.getElementById("addedWord").value;
+    if (added_Words.includes(word)) {
+        message.innerHTML = "The word was already added. Please insert another word!";
+        document.getElementById("addedWord").value = "";
+    } else {
+        message.innerHTML = "Word was added successfully!";
+        added_Words.push(word);
+        document.getElementById("addedWord").value = "";
+    }
+}
