b38b204369d2846d49871b00f083f53851c9cdee
Cristian Marinescu <97915515+CristianM92@users.noreply.github.com>
2022-06-08 22:10:32 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-let added_Words = [];
+let addedWords = [];
 function checkWords() {
     let word = document.getElementById("checkWord").value;
-    if (added_Words.includes(word)) {
+    if (addedWords.includes(word)) {
         message.innerHTML = "The word was already added. Please enter another word!";
         document.getElementById("checkWord").value = "";
     } else {
@@ -12,12 +12,12 @@ function checkWords() {
 
 function addWords() {
     let word = document.getElementById("addedWord").value;
-    if (added_Words.includes(word)) {
+    if (addedWords.includes(word)) {
         message.innerHTML = "The word was already added. Please insert another word!";
         document.getElementById("addedWord").value = "";
     } else {
         message.innerHTML = "Word was added successfully!";
-        added_Words.push(word);
+        addedWords.push(word);
         document.getElementById("addedWord").value = "";
     }
 }
